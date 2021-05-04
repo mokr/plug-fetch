d15981e069aeb9d336d0a5d208a181d3c6b41757
Morten Kristoffersen <morten.kristoffersen@ice.net>
2021-05-04 17:53:44 +0200

    Added group name (for clojars)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject plug-fetch "0.1.0-SNAPSHOT"
+(defproject mokr/plug-fetch "0.1.0-SNAPSHOT"
   :description "Simplify fetching of data from backend to frontend in a cljs app (experimental)"
   :url "https://github.com/mokr/plug-fetch"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
