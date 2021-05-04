7628705e4350fcc8df02ee9f31c708f013ff376f
Morten Kristoffersen <morten.kristoffersen@ice.net>
2021-05-04 16:22:59 +0200

    Initial commit (mostly template generated)

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject plug-fetch "0.1.0-SNAPSHOT"
+  :description "Simplify fetching of data from backend to frontend in a cljs app (experimental)"
+  :url "https://github.com/mokr/plug-fetch"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns plug-fetch.core})
