5565e26d20afec3afa33e15cbee44e5b67c4b71e
Morten Kristoffersen <morten.kristoffersen@ice.net>
2021-05-26 09:20:46 +0200

    :scope "provided" on dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/mokr/plug-fetch"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[cljs-ajax "0.8.3"]
-                 [org.clojure/clojure "1.10.3"]
+  :dependencies [[cljs-ajax "0.8.3" :scope "provided"]
+                 [org.clojure/clojure "1.10.3" :scope "provided"]
                  [org.clojure/clojurescript "1.10.844" :scope "provided"]
-                 [re-frame "1.2.0"]]
+                 [re-frame "1.2.0" :scope "provided"]]
   :repl-options {:init-ns plug-fetch.core})
